a07473a46a7053c8ffbf4b0ca59b90c69547ac79
三井 翔吾 <mitsui@queserser-j.co.jp>
2016-09-28 02:29:19 +0900

    first commit.

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+      }
+    ]
+  }
+};
